4658b3ab5f33b9394792fb2aa8f950d32adfaf42
Bryan Mytko <bryanmytko@gmail.com>
2016-07-22 19:02:15 -0400

    Add basic master login and oauth methods

--- lib/gpsoauth.rb
+++ lib/gpsoauth.rb
@@ -1,5 +1,21 @@
-require "gpsoauth/version"
+# require "gpsoauth/version"
 
 module Gpsoauth
-  # Your code goes here...
+  def master_login(email, password, android_id, service, device_country,
+                   operator_country, lang, sdk_version)
+
+    service ||= "acdm"
+    device_country ||= "us"
+    operator_country ||= "us"
+    lang ||= "en"
+    sdk_version ||= 17
+  end
+
+  def oauth
+  end
+
+  private
+
+  def auth_request
+  end
 end
