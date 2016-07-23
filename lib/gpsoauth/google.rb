996b453f1adcd7765c758b77d852f84625224cbb
Bryan Mytko <bryanmytko@gmail.com>
2016-07-23 12:58:50 -0400

    Finish Google#key_from_b64; OpenSSL::BN version

--- lib/gpsoauth/google.rb
+++ lib/gpsoauth/google.rb
@@ -3,16 +3,18 @@ module Gpsoauth
     def self.key_from_b64(key)
       binary_key = Base64.decode64(key)
 
-      # TODO Go to bed.
       i = binary_key[0,4].sum
-      mod = binary_key[4, DISTANCE_NOT_RANGE].sum
+      mod = binary_key[4, i].unpack("H*")[0].to_i(16)
 
-      j = binary_key[i + 4, DISTANCE_NOT_RANGE].sum
-      exponent = binary_key[i + 8, DISTANCE_NOT_RANGE].sum
 
-      debugger
-      v = OpenSSL::PKey::RSA.generate(mod, exponent)
+      j = binary_key[i + 4, 4].sum
+      exponent = binary_key[i + 8, j].unpack("H*")[0].to_i(16)
 
+      key = OpenSSL::PKey::RSA.new
+      key.e = OpenSSL::BN.new(exponent)
+      key.n = OpenSSL::BN.new(mod)
+
+      key
     end
 
     def self.signature(email, password, key)
