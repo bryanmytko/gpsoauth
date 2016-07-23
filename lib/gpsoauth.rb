504c5fe008fc006590ed8a0fb64da9b159756c93
Bryan Mytko <bryanmytko@gmail.com>
2016-07-23 00:24:19 -0400

    Add utility and google files

--- lib/gpsoauth.rb
+++ lib/gpsoauth.rb
@@ -1,14 +1,26 @@
-# require "gpsoauth/version"
+require "gpsoauth/version"
+require "gpsoauth/google"
+require "gpsoauth/utility"
 
 module Gpsoauth
-  def master_login(email, password, android_id, service, device_country,
-                   operator_country, lang, sdk_version)
+  # The key is distirbuted with Google Play Services.
+  # This one is from version 7.3.29.
+  B64_KEY_7_3_29 = "AAAAgMom/1a/v0lblO2Ubrt60J2gcuXSljGFQXgcyZWveWLEwo6prwgi3" \
+  "iJIZdodyhKZQrNWp5nKJ3srRXcUW+F1BD3baEVGcmEgqaLZUNBjm057pKRI16kB0YppeGx5qIQ" \
+  "5QjKzsR8ETQbKLNWgRY0QRNVz34kMJR3P/LgHax/6rmf5AAAAAwEAAQ==".b
 
-    service ||= "acdm"
-    device_country ||= "us"
-    operator_country ||= "us"
-    lang ||= "en"
-    sdk_version ||= 17
+
+  def master_login(email, password, android_id,
+                   service = nil, device_country = nil,
+                   operator_country = nil, lang = nil, sdk_version = nil)
+
+    service = service || "acdm"
+    device_country = device_country || "us"
+    operator_country = operator_country || "us"
+    lang = lang || "en"
+    sdk_version = sdk_version || 17
+
+    p B64_KEY_7_3_29
   end
 
   def oauth
---
+++ lib/gpsoauth/google.rb
@@ -0,0 +1,2 @@
+module Gpsoauth
+end
