dafd1e827bfb39be5a4907564cfc77976981453a
Bryan Mytko <bryanmytko@gmail.com>
2016-07-24 12:52:36 -0400

    Add Gpsoauth::Connection.parse_auth_response

--- lib/gpsoauth/connection.rb
+++ lib/gpsoauth/connection.rb
@@ -1,23 +1,19 @@
 module Gpsoauth
   class Connection
-    # The key is distirbuted with Google Play Services.
-    # This one is from version 7.3.29.
-
-    B64_KEY_7_3_29 = "AAAAgMom/1a/v0lblO2Ubrt60J2gcuXSljGFQXgcyZWveWLEwo6p" \
-    "rwgi3iJIZdodyhKZQrNWp5nKJ3srRXcUW+F1BD3baEVGcmEgqaLZUNBjm057pKRI16kB0" \
-    "YppeGx5qIQ5QjKzsR8ETQbKLNWgRY0QRNVz34kMJR3P/LgHax/6rmf5AAAAAwEAAQ==".b
-
     AUTH_URL = "https://android.clients.google.com/auth"
     USER_AGENT = "gpsoauth/#{VERSION}"
 
-    # @TODO REMOVE THIS, should be client provided
-    AID = "9774d56d682e549c"
+    # The key is distirbuted with Google Play Services.
+    # This one is from version 7.3.29.
+    B64_KEY_7_3_29 = "AAAAgMom/1a/v0lblO2Ubrt60J2gcuXSljGFQXgcyZWveWLEwo6p" \
+    "rwgi3iJIZdodyhKZQrNWp5nKJ3srRXcUW+F1BD3baEVGcmEgqaLZUNBjm057pKRI16kB0" \
+    "YppeGx5qIQ5QjKzsR8ETQbKLNWgRY0QRNVz34kMJR3P/LgHax/6rmf5AAAAAwEAAQ==".b
 
     def master_login(email, password, android_id,
                      service = nil, device_country = nil,
                      operator_country = nil, lang = nil, sdk_version = nil)
 
-      android_id = AID # @TODO Remove, client provided
+      android_id = "9774d56d682e549c" # @TODO Remove, client provided
       service = service || "ac2dm"
       device_country = device_country || "us"
       operator_country = operator_country || "us"
@@ -33,7 +29,7 @@ module Gpsoauth
         add_account: 1,
         Passwd: password,
         # EncryptedPasswd: Google::signature(email, password, android_key), # @TODO
-        # service: service,
+        service: service,
         source: "android",
         androidId: android_id,
         device_country:  device_country,
@@ -60,13 +56,11 @@ module Gpsoauth
       }
 
       response = HTTParty.post(AUTH_URL, options)
-
-      puts response
-      # Probably should parse the token, that's what Parse_auth_response needs
-      # @TODO Google::Parse_auth_response(response)
+      parse_auth_response(response)
     end
 
-    def android_key(b64_key)
+    def parse_auth_response(response)
+      Hash[response.each_line.map { |l| l.chomp.split("=", 2) }]
     end
   end
 end
